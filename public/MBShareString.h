075d3999c73321cad22d591bc30434b2c70ef3da
Michael Banack <bob5972@banack.net>
2011-12-17 00:56:26 -0800

    Inlined a bunch of MBShareString functions

--- public/MBShareString.h
+++ public/MBShareString.h
@@ -5,6 +5,7 @@
 #include <ostream>
 
 #include "MBString.h"
+#include "mbutil.h"
 
 using namespace std;
 
@@ -17,81 +18,228 @@ class MBShareString
 {
     public:
     	//Construct an empty string
-    	MBShareString();
-    	
-    	//constructs an empty string, with the specified buffer size
-    	explicit MBShareString(int size);
-    	
-    	//construct a string of length size, filled with fill
-        MBShareString(int size, char fill);
+    	MBShareString()
+    	:myRef(NULL)
+		{
+			myRef = new StringRef();
+		}
 
-        //Construct from C-String
-        MBShareString(const char * s);
+        MBShareString(int size, char fill)
+        :myRef(NULL)
+		{
+			myRef = new StringRef(size, fill);
+		}
+
+        MBShareString(int size)
+        :myRef(NULL)
+        {
+			myRef = new StringRef(size);
+		}
+
+        MBShareString(char x)
+        :myRef(NULL)
+		{
+			myRef = new StringRef(x);
+		}
+
+	    //Precondition: c is a null-terminated string, or c = NULL
+        MBShareString(const char * c)
+	    :myRef(NULL)
+	    {	
+	    	myRef = new StringRef(c);	
+	    }
+
+        MBShareString(const MBShareString & str)
+        :myRef(NULL)
+  	    {	
+	    	ASSERT(str.myRef != NULL);
+		    linkRef(str.myRef);
+		    ASSERT(myRef != NULL);
+	    }
+
+        MBShareString(const MBString & bstr)
+        :myRef(NULL) 
+	    {	
+	         myRef = new StringRef(bstr);
+	    }
+
+	    ~MBShareString()
+        {
+	         unlinkRef();
+        }
+
+        const MBShareString&  operator = (const MBShareString & str)
+        {
+			if (this != &str) {
+				linkRef(str.myRef);
+			}
+			
+			return *this;
+		}
+
+        const MBShareString&  operator = (const MBString & bstr)
+        {
+			if(myRef->refCount == 1) {
+				//reuse buffer if we have our own
+				myRef->str = bstr;
+			} else {
+				unlinkRef();
+				myRef = new StringRef(bstr);
+			}
+			
+			return *this;
+		}
+
+        //Precondition: c points to a null terminated string or c = NULL
+        const MBShareString & operator = (const char* c)
+		{
+			ASSERT(myRef != NULL);
+			
+			if(myRef->refCount > 1) {
+				unlinkRef();
+				myRef = new StringRef(c);
+			} else {
+				ASSERT(myRef->refCount == 1);
+				myRef->str = c;
+			}
+			
+			return *this;
+		}
         
-        MBShareString(const MBString & bstr);
-       	MBShareString(const MBShareString & mbstr);
-        MBShareString(char x);
+        const MBShareString & operator = (char c)
+        {
+			ASSERT(myRef != NULL);
+			
+			if(myRef->refCount > 1) {
+				unlinkRef();
+				myRef = new StringRef(c);
+			} else {
+				ASSERT(myRef->refCount == 1);
+				myRef->str = c;
+			}
+			
+			return *this;
+		}
 
-        ~MBShareString();
+        //Number of logical characters
+        int length() const
+		{
+			return myRef->str.length();	
+		}
 
+        //return as C-String
+        const char* cstr() const
+		{
+			return myRef->str.cstr();
+		}
+        
+        int find(char ta) const
+		{
+			return myRef->str.find(ta);
+		}
+        
+        int find(const MBShareString & mbstr) const
+		{
+			return myRef->str.find(mbstr.myRef->str);
+		}
 
-    	const MBShareString & operator = (const MBShareString & str);
-    	const MBShareString & operator = (const MBString & bstr);
-        const MBShareString & operator = (const char* s);
-        const MBShareString & operator = (char c);
-
-
-   	    //Number of Characters
-    	int length() const;
-
-   	    //return as C-String
-   	    //The returned string must NOT be freed by the caller
-   	    //The returned string may change if the
-   	    //    MBShareString is modified
-   	    const char * cstr() const;
-
-   	    //returns index of leftmost instance of x
-   	    int find(char x) const;
-   	    //returns index of leftmost instance of x
-   	    int find(const MBShareString & str) const;
-
-		//These return new strings
-   	    MBShareString substr(int pos, int len) const;
-   	    MBShareString toUpper() const;
-   	    MBShareString toLower() const;
-
-    	//Range-checked indexing
-    	char   operator[ ]( int k ) const;
-     	
+        MBShareString substr(int pos,int len) const
+		{
+			//you could get some extra mileage out of this
+			//  if you converted MBString into a pos,len model
+			//  and shared substrs with a single StringRef
+			
+			ASSERT(len >= 0);
+			
+			MBShareString oup(len);
+			
+			oup.myRef->str = myRef->str.substr(pos,len);
+			
+			return oup;
+		}
+        
+        MBShareString toUpper() const
+		{
+			MBShareString oup(length());
+			
+			oup.myRef->str = myRef->str.toUpper();
+			
+			return oup;
+		}
+        
+        MBShareString toLower() const
+		{
+			MBShareString oup(length());
+			
+			oup.myRef->str = myRef->str.toLower();
+			
+			return oup;
+		}
+        
+        char operator[ ]( int k ) const
+		{
+			return myRef->str[k];
+		}
      	//Range-checked indexing
      	//  Using this will assume you are writing to it
      	//  and trigger a copy if this is a shared reference
      	char & operator[ ]( int k );
    		
-
-        
         //Append str
         const MBShareString & operator += ( const MBShareString & str );
         
         //Append char
-        const MBShareString & operator += ( char ch );        
+        const MBShareString& operator += ( char ch )
+		{
+			myRef->str.append(ch);
+			
+			return *this;
+		}
+
+
         
         //like java's
         //  return >0 if this > rhs
         //  return  0 if this == rhs
         //  return <0 if this < rhs
-        int compareTo(const MBShareString& rhs) const;
+        int compareTo(const MBShareString& rhs) const
+		{
+			return myRef->str.compareTo(rhs.myRef->str);
+		}
 
     private:
     	class StringRef
     	{
     		public:
-    			StringRef();
-    			StringRef(int size, char fill);
-    			StringRef(int size);
-    			StringRef(char x);
-    			StringRef(const char* cstr);
-    			StringRef(const MBString& bstr);
+    			StringRef()
+    			:refCount(1),
+    			str()
+				{  }
+
+    			StringRef(int size, char fill)
+    			:refCount(1),
+    			str(size, fill)
+				{  }
+
+    			StringRef(char x)
+    			:refCount(1),
+    			str(x)
+				{  }
+
+		        StringRef(int size)
+		        :refCount(1),
+                str(size)
+				{  }
+
+                StringRef(const char* cstr)
+                :refCount(1),
+                str(cstr)
+                {  }
+
+    			StringRef(const MBString& bstr)
+    			:refCount(1),
+    			str(bstr)
+				{  }
     		
     			int refCount;
     			MBString str;
@@ -115,23 +263,113 @@ class MBShareString
 //IO Functions
 
 //IO Member Functions
-ostream& operator << ( ostream& os, const MBShareString& str );
-istream& operator >> ( istream& is, MBShareString& str );
-istream& getline( istream& is, MBShareString& str );
+static INLINE ostream& operator << ( ostream& os,
+                                     const MBShareString& str )
+{
+	return os << str.cstr();
+}
+
+//precondition:  input stream is open for reading
+//postcondition: the next string from input stream is has been read
+//               and stored in str
+
+static INLINE istream& operator >> ( istream& is,
+                                     MBShareString& str )
+{
+	//This should really be done with a member function
+	// to avoid a string copy, but I was feeling lazy
+    MBString bstr;
+    
+    is >> bstr;
+    
+    str = bstr;
+    
+    return is;
+}
+	
+//description:   reads a line from input stream is into the string str
+//precondition:  input stream is open for reading
+//postcondition: chars from input stream is up to '\n' have been read
+static INLINE istream& getline( istream& is,
+                                MBShareString& str )
+{
+
+    MBString bstr;
+    
+    getline(is, bstr);
+    
+    str = bstr;
+    
+    return is;
+}
 
 // comparison operators:
-bool operator == ( const MBShareString & lhs, const MBShareString & rhs );
-bool operator != ( const MBShareString & lhs, const MBShareString & rhs );
-bool operator <  ( const MBShareString & lhs, const MBShareString & rhs );
-bool operator <= ( const MBShareString & lhs, const MBShareString & rhs );
-bool operator >  ( const MBShareString & lhs, const MBShareString & rhs );
-bool operator >= ( const MBShareString & lhs, const MBShareString & rhs );
+static INLINE bool operator == ( const MBShareString & lhs,
+                                 const MBShareString & rhs )
+{
+	return lhs.compareTo(rhs) == 0;
+}
+
+static INLINE bool operator != ( const MBShareString & lhs,
+                                 const MBShareString & rhs )
+{
+	return lhs.compareTo(rhs) != 0;
+}
+
+static INLINE bool operator <  ( const MBShareString & lhs,
+                                 const MBShareString & rhs )
+{
+	return lhs.compareTo(rhs) < 0;
+}
+
+static INLINE bool operator <= ( const MBShareString & lhs,
+                                 const MBShareString & rhs )
+{
+	return lhs.compareTo(rhs) <= 0;
+}
+
+static INLINE bool operator >  ( const MBShareString & lhs,
+                                 const MBShareString & rhs )
+{
+	return lhs.compareTo(rhs) > 0;
+}
+
+static INLINE bool operator >= ( const MBShareString & lhs,
+                                 const MBShareString & rhs )
+{
+	return lhs.compareTo(rhs) >= 0;
+}
+
 
 // concatenation operator +
-MBShareString operator + ( const MBShareString & lhs, const MBShareString & rhs );
-MBShareString operator + ( char ch, const MBShareString & str );
-MBShareString operator + ( const MBShareString & str, char ch );
+static INLINE MBShareString operator + ( const MBShareString & lhs,
+                                         const MBShareString & rhs )
+{
+	MBShareString oup(lhs.length() + rhs.length());
+	
+	oup += lhs;
+	oup += rhs;
+	
+	return oup;
+}
 
+static INLINE MBShareString operator + ( char ch,
+                                         const MBShareString & str )
+{
+	MBShareString oup(1+str.length());
+	oup = ch;
+	oup += str;
+	return oup;
+}
+
+static INLINE MBShareString operator + ( const MBShareString & str,
+                                         char ch )
+{
+	MBShareString oup (1+str.length());
+	oup += str;
+	oup += ch;
+	return oup;
+}
 
 
 #endif //MBShareString_H_201001091354
--- test.cpp
+++ test.cpp
@@ -69,6 +69,7 @@ void testMBString(void)
 	result = cstr[11];
 	ASSERT(result == '!');
 
+	cstr = "Hello World!";
 	result = (cstr < "Hello Q");
 	ASSERT(!result);
 	result = (cstr > "Hello Q");
@@ -166,6 +167,18 @@ void testMBShareString(void)
 	result = cstr[11];
 	ASSERT(result == '!');
 	
+	cstr = "Hello World!";
+	result = (cstr < "Hello Q");
+	ASSERT(!result);
+	result = (cstr > "Hello Q");
+	ASSERT(result);
+	result = (cstr != "ABC");
+	ASSERT(result);
+	result = (cstr != "Hello Wozld!");
+	ASSERT(result);
+	result = (cstr == "Hello World!");
+	ASSERT(result);
+
 	str = "Goodbye World!";
 	str2 = str.substr(0, 1);
 	result = str2.length();
@@ -509,15 +522,15 @@ int main(int argc, char *argv[])
 	
 	BenchmarkTest tests[] = {
 		// enabled, weight, function
-		{ 1, 60000,  testMBString      },
+		{ 0, 60000,  testMBString      },
 		{ 1, 60000,  testMBShareString },
-		{ 1, 10000,  testMBVector      },
-		{ 1, 5000,   testMBStack       },
-		{ 1, 20,     testMBSet         },
-		{ 1, 800,    testIntSet        },
-		{ 1, 1300,   testBitVector     },
-		{ 1, 8000,   testMBMap         },
-		{ 1, 2700,   testIntMap        },
+		{ 0, 10000,  testMBVector      },
+		{ 0, 5000,   testMBStack       },
+		{ 0, 20,     testMBSet         },
+		{ 0, 800,    testIntSet        },
+		{ 0, 1300,   testBitVector     },
+		{ 0, 8000,   testMBMap         },
+		{ 0, 2700,   testIntMap        },
 	};
 	
 	//Functional tests
