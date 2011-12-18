57b379cdafc239987540e01c7a9f3a437e54408f
Michael Banack <bob5972@banack.net>
2011-12-18 15:35:39 -0800

    Moved CityHash into the main folder
    Also merged city.h and citycrc.h into cityhash.h

--- public/cityhash.h
+++ public/cityhash.h
@@ -40,6 +40,7 @@
 // of a+b is easily derived from the hashes of a and b.  This property
 // doesn't hold for any hash functions in this file.
 
+
 #ifndef CITY_HASH_H_
 #define CITY_HASH_H_
 
@@ -88,3 +89,24 @@ inline uint64 Hash128to64(const uint128& x) {
 }
 
 #endif  // CITY_HASH_H_
+
+// Originally taken from citycrc.h
+// This file also declares the subset of the CityHash functions that require
+// _mm_crc32_u64().  See the CityHash README for details.
+//
+// Functions in the CityHash family are not suitable for cryptography.
+
+#ifndef CITY_HASH_CRC_H_
+#define CITY_HASH_CRC_H_
+
+// Hash function for a byte array.
+uint128 CityHashCrc128(const char *s, size_t len);
+
+// Hash function for a byte array.  For convenience, a 128-bit seed is also
+// hashed into the result.
+uint128 CityHashCrc128WithSeed(const char *s, size_t len, uint128 seed);
+
+// Hash function for a byte array.  Sets result[0] ... result[3].
+void CityHashCrc256(const char *s, size_t len, uint64 *result);
+
+#endif  // CITY_HASH_CRC_H_
