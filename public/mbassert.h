833be958f41c9cdd254bd2b924587278dd8845c7
Michael Banack <bob5972@banack.net>
2016-02-20 17:06:42 -0800

    formatting

--- public/mbassert.h
+++ public/mbassert.h
@@ -19,7 +19,7 @@
 /*
  * mbassert.h --
  */
- 
+
 #ifndef MBASSERT_H_201001091250
 #define MBASSERT_H_201001091250
 
@@ -30,34 +30,35 @@
 
 #ifdef __cplusplus
 	extern "C" {
-#endif 
-
-#if MB_DEBUG
-	#ifndef MB_ASSERT
-		#define MB_ASSERT 1
-	#endif
 #endif
 
+#if MB_DEBUG
 #ifndef MB_ASSERT
-	#define MB_ASSERT 0
+#define MB_ASSERT 1
+#endif //MB_ASSERT
+#endif // MB_DEBUG
+
+#ifndef MB_ASSERT
+#define MB_ASSERT 0
 #endif
 
 #if MB_ASSERT
-	#define ASSERT(x) do { \
-		if (UNLIKELY(!(x))) { \
-			PanicAssertFail(__FILE__, __LINE__, #x); \
-		} \
-	} while (FALSE)
-	
+#define ASSERT(x)
+    do { \
+        if (UNLIKELY(!(x))) { \
+                PanicAssertFail(__FILE__, __LINE__, #x); \
+        } \
+    } while (FALSE)
 #else
-	#define ASSERT(x)
+#define ASSERT(x)
 #endif
 
-#define VERIFY(x) do { \
-		if (UNLIKELY(!(x))) { \
-			PanicWithMessage(__FILE__, __LINE__, "VERIFY FAILED"); \
-		} \
-	} while (FALSE)
+#define VERIFY(x) \
+    do { \
+        if (UNLIKELY(!(x))) { \
+                PanicWithMessage(__FILE__, __LINE__, "VERIFY FAILED"); \
+        } \
+    } while (FALSE)
 
 #define NOT_REACHED() PanicWithMessage( __FILE__, __LINE__, "NOT_REACHED")
 
@@ -71,6 +72,6 @@ NORETURN void Panic(void);
 
 #ifdef __cplusplus
 	}
-#endif 
+#endif
 
 #endif //MBASSERT_H_201001091250
