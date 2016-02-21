2e21bc429b761e093d51d408f16768145de3da83
Michael Banack <bob5972@banack.net>
2016-02-20 17:00:31 -0800

    Indented Panic

--- mbassert.c
+++ mbassert.c
@@ -19,7 +19,7 @@
 /*
  * mbassert.c --
  */
- 
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,27 +29,27 @@
 
 NORETURN void PanicWithMessage(const char *file, int line, const char *fmt, ...)
 {
-	va_list args;
-	
-	va_start(args, fmt);
-	fprintf(stderr, "%s:%d| ", file, line);
-	fprintf(stderr, "PANIC: ");
-	vfprintf(stderr, fmt, args);
-	fprintf(stderr, "\n");
-	va_end(args);
-	Panic();
+    va_list args;
+
+    va_start(args, fmt);
+    fprintf(stderr, "%s:%d| ", file, line);
+    fprintf(stderr, "PANIC: ");
+    vfprintf(stderr, fmt, args);
+    fprintf(stderr, "\n");
+    va_end(args);
+    Panic();
 }
 
 NORETURN void PanicAssertFail(const char *file, int line, const char *cond)
 {
-	fprintf(stderr, "%s:%d| ", file, line);
-	fprintf(stderr, "ASSERT: %s\n", cond);
-	Panic();
+    fprintf(stderr, "%s:%d| ", file, line);
+    fprintf(stderr, "ASSERT: %s\n", cond);
+    Panic();
 }
 
 NORETURN void Panic(void)
 {
-	exit(1);
+    exit(1);
 }
 
 
