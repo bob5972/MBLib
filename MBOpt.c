3415623d257031d9762992f9ee2d584771ea08ac
Michael Banack <bob5972@banack.net>
2021-10-30 08:22:35 -0700

    Update Copyright dates

--- MBOpt.c
+++ MBOpt.c
@@ -1,7 +1,7 @@
 /*
  * MBOpt.c -- part of MBLib
  *
- * Copyright (c) 2020 Michael Banack <github@banack.net>
+ * Copyright (c) 2020-2021 Michael Banack <github@banack.net>
  *
  * MIT License
  * Permission is hereby granted, free of charge, to any person obtaining a copy
